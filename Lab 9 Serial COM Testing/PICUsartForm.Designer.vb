4ec74ce5f2d1d57586cf2c67b7805919e19efeef
Zac <zac.minidude140@Gmail.com>
2024-10-22 12:11:16 -0600

    Started Adding Serial COM

--- Lab 9 Serial COM Testing/PICUsartForm.Designer.vb
+++ Lab 9 Serial COM Testing/PICUsartForm.Designer.vb
@@ -22,7 +22,13 @@ Partial Class PICUsartForm
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Me.ExitButton = New System.Windows.Forms.Button()
+        Me.ServoPosComboBox = New System.Windows.Forms.ComboBox()
+        Me.ServoPosSelectLabel = New System.Windows.Forms.Label()
+        Me.SetServoPosButton = New System.Windows.Forms.Button()
+        Me.TestLabel = New System.Windows.Forms.Label()
+        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
         Me.SuspendLayout()
         '
         'ExitButton
@@ -35,17 +41,63 @@ Partial Class PICUsartForm
         Me.ExitButton.Text = "Exit"
         Me.ExitButton.UseVisualStyleBackColor = True
         '
-        'Form1
+        'ServoPosComboBox
+        '
+        Me.ServoPosComboBox.FormattingEnabled = True
+        Me.ServoPosComboBox.Items.AddRange(New Object() {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"})
+        Me.ServoPosComboBox.Location = New System.Drawing.Point(520, 141)
+        Me.ServoPosComboBox.Name = "ServoPosComboBox"
+        Me.ServoPosComboBox.Size = New System.Drawing.Size(158, 24)
+        Me.ServoPosComboBox.TabIndex = 1
+        '
+        'ServoPosSelectLabel
+        '
+        Me.ServoPosSelectLabel.AutoSize = True
+        Me.ServoPosSelectLabel.Location = New System.Drawing.Point(527, 121)
+        Me.ServoPosSelectLabel.Name = "ServoPosSelectLabel"
+        Me.ServoPosSelectLabel.Size = New System.Drawing.Size(142, 17)
+        Me.ServoPosSelectLabel.TabIndex = 2
+        Me.ServoPosSelectLabel.Text = "Select Servo Position"
+        '
+        'SetServoPosButton
+        '
+        Me.SetServoPosButton.Location = New System.Drawing.Point(521, 186)
+        Me.SetServoPosButton.Name = "SetServoPosButton"
+        Me.SetServoPosButton.Size = New System.Drawing.Size(156, 54)
+        Me.SetServoPosButton.TabIndex = 3
+        Me.SetServoPosButton.Text = "Send Servo Data"
+        Me.SetServoPosButton.UseVisualStyleBackColor = True
+        '
+        'TestLabel
+        '
+        Me.TestLabel.AutoSize = True
+        Me.TestLabel.Location = New System.Drawing.Point(573, 279)
+        Me.TestLabel.Name = "TestLabel"
+        Me.TestLabel.Size = New System.Drawing.Size(51, 17)
+        Me.TestLabel.TabIndex = 4
+        Me.TestLabel.Text = "Label1"
+        '
+        'PICUsartForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.TestLabel)
+        Me.Controls.Add(Me.SetServoPosButton)
+        Me.Controls.Add(Me.ServoPosSelectLabel)
+        Me.Controls.Add(Me.ServoPosComboBox)
         Me.Controls.Add(Me.ExitButton)
-        Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.Name = "PICUsartForm"
+        Me.Text = "USART PIC COM"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents ExitButton As Button
+    Friend WithEvents ServoPosComboBox As ComboBox
+    Friend WithEvents ServoPosSelectLabel As Label
+    Friend WithEvents SetServoPosButton As Button
+    Friend WithEvents TestLabel As Label
+    Friend WithEvents SerialPort1 As IO.Ports.SerialPort
 End Class
--- Lab 9 Serial COM Testing/PICUsartForm.vb
+++ Lab 9 Serial COM Testing/PICUsartForm.vb
@@ -4,16 +4,57 @@
 'Testing USART With PIC16F883
 'GitHub: 
 
-
+Imports System.Threading
 Public Class PICUsartForm
+    Dim txData As Byte
     '*****************Custom Methods******************************
+    ''' <summary>
+    ''' Sets the Default Combo Box Selection
+    ''' </summary>
+    Sub SetDefaults()
+        ServoPosComboBox.SelectedIndex = 0
+    End Sub
 
+    ''' <summary>
+    ''' Sets Port Name, Baud, and Opens Port for Communication
+    ''' </summary>
+    Sub OpenCOM()
+        'Set up port name and baud rate
+        SerialPort1.PortName = "COM8"
+        SerialPort1.BaudRate = 9600
+        'open serial port for data transfer
+        SerialPort1.Open()
+    End Sub
 
-
+    Sub SendData(txData As Byte)
+        Dim data() As Byte
+    End Sub
 
 
     '****************Event Handlers*************************
+    Private Sub PICUsartForm_Load(sender As Object, e As EventArgs) Handles Me.Load
+        SetDefaults()
+    End Sub
+
     Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
         Me.Close()
     End Sub
+
+    Private Sub SetServoPosButton_Click(sender As Object, e As EventArgs) Handles SetServoPosButton.Click
+        'Create Variable For Data Storage
+        Dim data(0) As Byte
+        'Set Data as an ASCII $; 0x24
+        data(0) = &H24
+        'Send $ 
+        SerialPort1.Write(data, 0, 1)
+        'Wait For Response
+        Thread.Sleep(100)
+        'Read Acknowledge Byte
+        SerialPort1.Read(data, 0, 1)
+
+
+        'txData = CInt(ServoPosComboBox.SelectedItem)
+
+    End Sub
+
 End Class
