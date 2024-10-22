abe5faf9d7f2dc9b4f8d436a34e7e524add25989
Zac <zac.minidude140@Gmail.com>
2024-10-22 11:24:58 -0600

    Add project files.

---
+++ Lab 9 Serial COM Testing/PICUsartForm.vb
@@ -0,0 +1,19 @@
+﻿'Zachary Christensen
+'RCET 5th Semester Lab
+'10/22/24
+'Testing USART With PIC16F883
+'GitHub: 
+
+
+Public Class PICUsartForm
+    '*****************Custom Methods******************************
+
+
+
+
+
+    '****************Event Handlers*************************
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Me.Close()
+    End Sub
+End Class
